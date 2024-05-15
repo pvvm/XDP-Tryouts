29457f61e0bf1c2ddd58d5ba89073ba031fabf87
Pedro Mizuno <pvvalenc@aqua08.syn.uwaterloo.ca>
2024-05-15 01:09:17 +0000

    Updating script

--- user_kernel_com/update_bpftool.sh
+++ user_kernel_com/update_bpftool.sh
@@ -1,3 +1,10 @@
+# Install packages
+sudo apt install clang llvm libelf-dev libpcap-dev build-essential libc6-dev-i386
+sudo apt install linux-tools-$(uname -r)
+sudo apt install linux-headers-$(uname -r)
+sudo apt install linux-tools-common linux-tools-generic
+sudo apt install tcpdump
+
 # Download bpftool 7.2.0
 wget https://github.com/libbpf/bpftool/releases/download/v7.2.0/bpftool-v7.2.0-amd64.tar.gz
 
