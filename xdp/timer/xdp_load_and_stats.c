7e6121617e1a5ca4afc8a5a647076cf1cdde8352
Pedro Mizuno <pvvalenc@aqua07.syn.uwaterloo.ca>
2024-06-02 21:07:07 +0000

    Changing folder organization and adding timer example

---
+++ xdp/timer/xdp_load_and_stats.c
@@ -0,0 +1,196 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+static const char *__doc__ = "XDP loader and stats program\n"
+	" - Allows selecting BPF --progname name to XDP-attach to --dev\n";
+
+#define _GNU_SOURCE /* needed to the the CPU id */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <signal.h>
+
+#include <locale.h>
+#include <unistd.h>
+#include <time.h>
+#include <sched.h> /* needed to the the CPU id */
+#include <pthread.h>
+
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <xdp/libxdp.h>
+
+#include <net/if.h>
+#include <linux/if_link.h> /* depend on kernel-headers installed */
+#include <linux/bpf.h>
+
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+#include <arpa/inet.h>  // For htons() and inet_addr()
+
+#include "../common/common_params.h"
+#include "../common/common_user_bpf_xdp.h"
+
+#define MAX_NUMBER_CORES 8
+
+static const char *default_filename = "xdp_prog_kern.o";
+
+static const char *default_progname = "testing_timer";
+
+bool stop = false;
+
+
+struct config cfg = {
+		.ifindex   = -1,
+		.do_unload = false,
+};
+
+static const struct option_wrapper long_options[] = {
+	{{"help",        no_argument,		NULL, 'h' },
+	 "Show help", false},
+
+	{{"dev",         required_argument,	NULL, 'd' },
+	 "Operate on device <ifname>", "<ifname>", true},
+
+	{{"skb-mode",    no_argument,		NULL, 'S' },
+	 "Install XDP program in SKB (AKA generic) mode"},
+
+	{{"native-mode", no_argument,		NULL, 'N' },
+	 "Install XDP program in native mode"},
+
+	{{"auto-mode",   no_argument,		NULL, 'A' },
+	 "Auto-detect SKB or native mode"},
+
+	{{"unload",      required_argument,	NULL, 'U' },
+	 "Unload XDP program <id> instead of loading", "<id>"},
+
+	{{"unload-all",  no_argument,           NULL,  4  },
+	 "Unload all XDP programs on device"},
+
+	{{"quiet",       no_argument,		NULL, 'q' },
+	 "Quiet mode (no output)"},
+
+	{{"filename",    required_argument,	NULL,  1  },
+	 "Load program from <file>", "<file>"},
+
+	{{"progname",    required_argument,	NULL,  2  },
+	 "Load program from function <name> in the ELF file", "<name>"},
+
+	{{0, 0, NULL,  0 }}
+};
+
+int find_map_fd(struct bpf_object *bpf_obj, const char *mapname)
+{
+	struct bpf_map *map;
+	int map_fd = -1;
+
+	/* Lesson#3: bpf_object to bpf_map */
+	map = bpf_object__find_map_by_name(bpf_obj, mapname);
+        if (!map) {
+		fprintf(stderr, "ERR: cannot find map by name: %s\n", mapname);
+		goto out;
+	}
+
+	map_fd = bpf_map__fd(map);
+ out:
+	return map_fd;
+}
+
+static void exit_application(int signal)
+{
+	stop = true;
+	int err;
+
+	cfg.unload_all = true;
+	err = do_unload(&cfg);
+	if (err) {
+		fprintf(stderr, "Couldn't detach XDP program on iface '%s' : (%d)\n",
+			cfg.ifname, err);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	struct xdp_program *program;
+	char errmsg[1024];
+	int err;
+
+	/* Global shutdown handler */
+	signal(SIGINT, exit_application);
+	
+	/* Set default BPF-ELF object file and BPF program name */
+	strncpy(cfg.filename, default_filename, sizeof(cfg.filename));
+	strncpy(cfg.progname,  default_progname,  sizeof(cfg.progname));
+	/* Cmdline options can change progname */
+	parse_cmdline_args(argc, argv, long_options, &cfg, __doc__);
+
+	/* Required option */
+	if (cfg.ifindex == -1) {
+		fprintf(stderr, "ERR: required option --dev missing\n");
+		usage(argv[0], __doc__, long_options, (argc == 1));
+		return EXIT_FAIL_OPTION;
+	}
+
+        /* Unload a program by prog_id, or
+         * unload all programs on net device
+         */
+	if (cfg.do_unload || cfg.unload_all) {
+		err = do_unload(&cfg);
+		if (err) {
+			libxdp_strerror(err, errmsg, sizeof(errmsg));
+			fprintf(stderr, "Couldn't unload XDP program %d: %s\n",
+				cfg.prog_id, errmsg);
+			return err;
+		}
+
+		printf("Success: Unloading XDP prog name: %s\n", cfg.progname);
+		return EXIT_OK;
+	}
+
+	program = load_bpf_and_xdp_attach(&cfg);
+	if (!program)
+		return EXIT_FAIL_BPF;
+
+	if (verbose) {
+		printf("Success: Loaded BPF-object(%s) and used section(%s)\n",
+		       cfg.filename, cfg.progname);
+		printf(" - XDP prog id:%d attached on device:%s(ifindex:%d)\n",
+		       xdp_program__id(program), cfg.ifname, cfg.ifindex);
+	}
+
+	int prog_fd = xdp_program__fd(program);
+
+	if (prog_fd < 0)
+		printf("Error: xdp_program__fd failed\n\n");
+	else
+		printf("File descriptor: %d\n\n", xdp_program__fd(program));
+
+	int map_fd = find_map_fd(xdp_program__bpf_obj(program), "timer_array");
+	if (map_fd < 0) {
+		return EXIT_FAIL_BPF;
+	}
+
+	/*while(!stop) {
+		printf("%d", stop);
+	}
+	printf("%d", stop);*/
+
+	int i = 0;
+	while(i == 0) {
+		(void)!scanf("%d", &i);
+		//if(scanf("%d", &i) == -1)
+		//	printf("something");
+	}
+
+	cfg.unload_all = true;
+	err = do_unload(&cfg);
+	if (err) {
+		fprintf(stderr, "Couldn't detach XDP program on iface '%s' : (%d)\n",
+			cfg.ifname, err);
+	}
+
+    return 0;
+}
---
+++ xdp/timer/xdp_prog_kern.c
@@ -0,0 +1,124 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <linux/in.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+#include <time.h>
+
+#include "../common/parsing_helpers.h"
+
+#define MAX_NUMBER_CORES 8
+
+#define ONE_SEC 1000000000
+#define TEN_SEC 10000000000
+
+struct map_elem {
+    int value;
+    struct bpf_timer timer;
+    //struct xdp_md *packet;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, __u32);
+    __type(value, struct map_elem);
+    __uint(max_entries, MAX_NUMBER_CORES);
+} timer_array SEC(".maps");
+
+struct test_elem {
+    int test_val;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, __u32);
+    __type(value, struct test_elem);
+    __uint(max_entries, MAX_NUMBER_CORES);
+} test_array SEC(".maps");
+
+static void test_function() {
+    bpf_printk("TESTING FUNCTION CALL");
+}
+
+int testing_timer(struct xdp_md *ctx);
+
+static int function(void *map, __u32 *key, struct map_elem *val) {
+    struct map_elem *entry = bpf_map_lookup_elem(map, key);
+    if(!entry) {
+        bpf_printk("Couldn't get entry of the map");
+        return 0;
+    }
+
+    // Testing if you can access/alter other maps
+    /*struct test_elem *test = bpf_map_lookup_elem(&test_array, key);
+    if(!test) {
+        bpf_printk("Couldn't get entry of the map");
+        return 0;
+    }
+    bpf_printk("TEST MAP: %d", test->test_val);
+    test->test_val = entry->value;*/
+
+    if(entry->value < 10) {
+        bpf_printk("AFTER ENTRY: %d", entry->value);
+        bpf_printk("AFTER VAL: %d", val->value);
+        val->value = val->value + 1;
+        // NOTE: Try if we can call testing_timer with a new packet
+        test_function();
+        //testing_timer(entry->packet);
+
+        bpf_timer_start(&entry->timer, ONE_SEC, 0);
+    }
+
+    return 0;
+}
+
+SEC("xdp")
+int testing_timer(struct xdp_md *ctx)
+{
+    int cpu = bpf_get_smp_processor_id();
+
+    struct map_elem *map_entry;
+
+    map_entry = bpf_map_lookup_elem(&timer_array, &cpu);
+    if(!map_entry) {
+        bpf_printk("Couldn't get entry of the map");
+        return XDP_DROP;
+    }
+
+    //map_entry->value = 0;
+    //if(map_entry->value == 0)
+    //    map_entry->packet = ctx;
+
+    //void *data_end = (void *)(long)ctx->data_end;
+	//void *data = (void *)(long)ctx->data;
+
+    bpf_printk("BEFORE: %d", map_entry->value);
+    //bpf_printk("PKT LENGTH: %d", data_end);
+    //bpf_printk("PKT LENGTH: %d", data);
+
+    // Initializes the timer from a certain map using a specific clock
+    long int i = bpf_timer_init(&map_entry->timer, &timer_array, CLOCK_BOOTTIME);
+    if(i != 0)
+        bpf_printk("Error while initializing timer: %ld", i);
+
+    // Sets the function to be called after the timer triggers
+    i = bpf_timer_set_callback(&map_entry->timer, function);
+    if(i != 0)
+        bpf_printk("Error while setting callback: %ld", i);
+
+    // Starts the timer for 1 second
+    bpf_timer_start(&map_entry->timer, ONE_SEC, 0);
+
+    // Cancels the timer (if the 1 second hasn't passed)
+    //bpf_timer_cancel(&map_entry->timer);
+
+    bpf_printk("TEST");
+
+    return 0;
+}
+
+char _license[] SEC("license") = "GPL";
